ffb967ef9681e283e7d1488bedf3593ab4aa9cbf
snsinfu <snsinfu@gmail.com>
2019-01-28 14:51:20 +0900

    Change entrypoint script to mount /srv/users as /home

--- image/init.sh
+++ image/init.sh
@@ -5,17 +5,15 @@
 cat /srv/hosts > /etc/hosts
 cat /srv/resolv.conf > /etc/resolv.conf
 
-# Create users
-for user in $(ls -1 /srv/users); do
-    adduser -D -s /bin/sh ${user} || true
+# Create users.
+rmdir /home
+ln -s /srv/users /home
+
+for user in $(ls /home); do
+    adduser -D -s /bin/sh ${user}
     addgroup ${user} ssh_users
-
-    srcdir="/srv/users/${user}"
-    sshdir="/home/${user}/.ssh"
-
-    install -o ${user} -g ${user} -m 0700 -d "${sshdir}"
-    install -o ${user} -g ${user} -m 0600 "${srcdir}/authorized_keys" "${sshdir}"
+    chown -R ${user}:${user} "/home/${user}"
 done
 
-# Execute Docker CMD
+# Execute Docker CMD.
 exec "$@"
